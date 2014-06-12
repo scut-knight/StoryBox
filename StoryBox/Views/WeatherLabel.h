4c38809ac1b2a68ed6e2306f36c7f3835c68344b
binss <babywolf1@163.com>
2014-06-12 15:36:56 +0800

    解决证书绑定问题

---
+++ StoryBox/Views/WeatherLabel.h
@@ -0,0 +1,23 @@
+//
+//  WeatherLabel.h
+//  故事盒子
+//
+//  Created by bin on 14-6-12.
+//  Copyright (c) 2014年 sony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MKMapView.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface WeatherLabel : UIView
+{
+    CLLocationManager *_currentLoaction;
+    NSString * _country;
+    NSString * _city;
+}
+
+-(void)getLocation;
+
+
+@end
